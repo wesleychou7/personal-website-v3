b615682a67e62fe12c394065f070f97a8d1973b7
Wesley Chou <wesleychou9851@gmail.com>
2025-06-14 22:05:42 -0700

    initial commit

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata, Viewport } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Wesley Chou",
+  description: "Wesley Chou's personal website",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ my-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center">
+      <div className="border-2 border-red-500 w-full md:w-1/3">
+        <div>Wesley Chou</div>
+        <div>Social Media</div>
+      </div>
+    </div>
+  );
+}
