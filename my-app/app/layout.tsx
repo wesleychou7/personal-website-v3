8b9579114c4dd54bd531b743ded9637b71f2cc47
Wesley Chou <wesleychou9851@gmail.com>
2025-08-27 09:22:56 -0700

    add analytics

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -33,7 +33,9 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
